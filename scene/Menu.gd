7b1c03b7b590c7eb806d95c4949ed1c832bad76d
BragdonD <thomasduclos1012@gmail.com>
2023-11-23 17:35:39 +0100

    refactor: make it less messy with the scene and assets

--- scene/Menu.gd
+++ scene/Menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_enter_pressed():
-	get_tree().change_scene_to_file("res://game.tscn")
+	get_tree().change_scene_to_file("res://scene/game.tscn")
 
 
 func _on_options_pressed():
