3d897f05aafe643e866260ce9cbf12dd309e429d
BragdonD <thomasduclos1012@gmail.com>
2023-11-24 07:04:50 +0100

    feat: add task skeleton

---
+++ scene/tasks/Task.gd
@@ -0,0 +1,18 @@
+extends Node
+
+var task_button: Button
+var task_timer: Timer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_button_pressed():
+	pass
+	
+func _on_timer_timeout():
+	pass
---
+++ scene/tasks/tasks_manager/TasksManager.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var tasks: Array = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func add_task(task_name: String, time_to_complete: float):
+	pass
