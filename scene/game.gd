791e12ae30aa511bc82ae3e95180629fe0b088ef
BragdonD <thomasduclos1012@gmail.com>
2023-11-30 11:11:12 +0100

    feat: add plant animation

---
+++ scene/game.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$MarginContainer/VBoxContainer/PlantContainer/Panel/Node2D/AnimatedSprite2D.play("display")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_animated_sprite_2d_animation_finished():
+	print_debug("finished") # Replace with function body.
