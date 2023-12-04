9f8d3a9f5790405489d5c317717afbf2f5ff64d2
BragdonD <thomasduclos1012@gmail.com>
2023-12-04 08:51:12 +0100

    feat: make the plant animation autoplay in the game scene

--- scene/game.gd
+++ scene/game.gd
@@ -1,15 +1,33 @@
 extends Control
 
+var animation_timer := Timer.new()
+var animatedSprite2D : AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$MarginContainer/VBoxContainer/PlantContainer/Panel/Node2D/AnimatedSprite2D.play("state-3")
+	set_process_input(true)
+	
+	animatedSprite2D = $MarginContainer/VBoxContainer/PlantContainer/Panel/Node2D/AnimatedSprite2D
+	
+	# Connect signals
+	animation_timer.timeout.connect(_on_animation_timer_timeout)
+	
+	add_child(animation_timer)
 
+	# Start the animation
+	animatedSprite2D.play()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
+func _on_animated_sprite_2d_animation_looped():
+	# Stop the animation
+	animatedSprite2D.stop()
+	# Start the timer to delay the next animation start
+	animation_timer.start(5.0)  # 5 seconds delay
+	animation_timer.paused = false
+	animation_timer.one_shot = true
 
-func _on_animated_sprite_2d_animation_finished():
-	print_debug("finished") # Replace with function body.
+func _on_animation_timer_timeout():
+	# Restart the animation after the timer expires
+	animatedSprite2D.play()
