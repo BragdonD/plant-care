a6c72a936d999183d80b16fbb498e9d97aca203a
BragdonD <thomasduclos1012@gmail.com>
2023-12-04 07:45:42 +0100

    feat: add all plant animation as sprite sheets

--- scene/game.gd
+++ scene/game.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$MarginContainer/VBoxContainer/PlantContainer/Panel/Node2D/AnimatedSprite2D.play("display")
+	$MarginContainer/VBoxContainer/PlantContainer/Panel/Node2D/AnimatedSprite2D.play("state-3")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
