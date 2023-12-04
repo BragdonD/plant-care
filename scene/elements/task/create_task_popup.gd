0b3602b2c507ee5d2c40d31455d94c225220cd1a
BragdonD <thomasduclos1012@gmail.com>
2023-12-04 13:32:38 +0100

    fix: caret position wrong position when writing

--- scene/elements/task/create_task_popup.gd
+++ scene/elements/task/create_task_popup.gd
@@ -20,15 +20,17 @@ func _on_timer_time_text_changed(new_text):
 
 func _on_task_description_text_changed():
 	var new_text = $Panel/VBoxContainer/MarginContainer2/VBoxContainer/DescriptionContainer/TaskDescription.text
-	new_text = new_text.upper()
+	new_text = new_text.to_upper()
 	$Panel/VBoxContainer/MarginContainer2/VBoxContainer/DescriptionContainer/TaskDescription.text = new_text
 	task_description = new_text
+	$Panel/VBoxContainer/MarginContainer2/VBoxContainer/DescriptionContainer/TaskDescription.caret_column = len(new_text)
 
 
 func _on_task_name_text_changed(new_text):
-	new_text = new_text.upper()
+	new_text = new_text.to_upper()
 	task_name = new_text
 	$Panel/VBoxContainer/MarginContainer2/VBoxContainer/NameContainer/TaskName.text = new_text
+	$Panel/VBoxContainer/MarginContainer2/VBoxContainer/NameContainer/TaskName.caret_column = len(task_name)
 
 func _on_timer_needed_toggled(button_pressed):
 	if button_pressed:
@@ -40,3 +42,5 @@ func _on_timer_needed_toggled(button_pressed):
 func _on_create_pressed():
 	# create task and add it to task manager
 	pass # Replace with function body.
+
+
--- scene/game.gd
+++ scene/game.gd
@@ -2,18 +2,16 @@ extends Control
 
 var animation_timer := Timer.new()
 var animatedSprite2D : AnimatedSprite2D
+var createTaskPopUp: Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_process_input(true)
-	
 	animatedSprite2D = $MarginContainer/VBoxContainer/PlantContainer/Panel/Node2D/AnimatedSprite2D
-	
+	createTaskPopUp = $create_task_popup
 	# Connect signals
 	animation_timer.timeout.connect(_on_animation_timer_timeout)
-	
 	add_child(animation_timer)
-
 	# Start the animation
 	animatedSprite2D.play()
 
@@ -31,3 +29,7 @@ func _on_animated_sprite_2d_animation_looped():
 func _on_animation_timer_timeout():
 	# Restart the animation after the timer expires
 	animatedSprite2D.play()
+
+
+func _on_create_task_pressed():
+	createTaskPopUp.visible = true
